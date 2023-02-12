5a9335e7052fe92191a47a1b7d0d30dd68c9fe52
shomrkm <shochan1985@gmail.com>
2023-02-12 14:54:59 +0900

    chore: Install eslint plugins.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
-import Head from 'next/head'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from 'next/head';
+import { Inter } from '@next/font/google';
+import styles from '@/styles/Home.module.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
@@ -12,8 +12,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className='text-3xl font bold bg-red-100 p-4 round-sm'>Openlayers Samples</div>
+        <div className="text-3xl font bold bg-red-100 p-4 round-sm">Openlayers Samples</div>
       </main>
     </>
-  )
+  );
 }
