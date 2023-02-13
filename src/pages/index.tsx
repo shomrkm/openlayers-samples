8a539b4a481510bfbf4f1ab18b6bfabcd7fd932e
shomrkm <shochan1985@gmail.com>
2023-02-14 07:46:24 +0900

    feat: Remove unused css.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import { Inter } from '@next/font/google';
-import styles from '@/styles/Home.module.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -11,7 +10,7 @@ export default function Home() {
         <title>Openlayers Samples</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <main>
         <div className="text-3xl font bold bg-red-100 p-4 round-sm">Openlayers Samples</div>
       </main>
     </>
