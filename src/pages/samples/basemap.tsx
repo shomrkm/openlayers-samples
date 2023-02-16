411d930077c49a9b3c59b0e8804331fd562468c3
shomrkm <shochan1985@gmail.com>
2023-02-17 08:17:35 +0900

    feat: (wip) Creating a basemap page.

--- src/pages/samples/basemap.tsx
+++ src/pages/samples/basemap.tsx
@@ -1,28 +1,28 @@
-import Head from 'next/head';
 import React, { useEffect, useRef } from 'react';
 
-import { FullScreen, Zoom, ZoomSlider } from 'ol/control';
+import 'ol/ol.css';
 import TileLayer from 'ol/layer/Tile';
 import OLMap from 'ol/Map';
 import OSM from 'ol/source/OSM';
 import View from 'ol/View';
 
 const BaseMap: React.FC = () => {
-  const mapRef = useRef<any>(null);
+  const mapElement = useRef<HTMLDivElement>(null);
+  const mapRef = useRef<OLMap>();
 
   useEffect(() => {
-    if (!mapRef) return;
-    new OLMap({
-      target: mapRef.current,
+    if (!mapElement.current || mapRef.current) return;
+    mapRef.current = new OLMap({
+      target: mapElement.current,
       view: new View({ projection: 'CRS:84', zoom: 16, center: [141.1378, 39.6987] }),
       layers: [new TileLayer({ source: new OSM() })],
-      controls: [new FullScreen(), new Zoom(), new ZoomSlider()],
     });
   }, []);
 
   return (
-    <div className="flex flex-col h-[600px] w-[600px]" ref={mapRef}>
-      {/* <div className="justify-start" ref={mapRef} /> */}
+    <div className="flex-col">
+      <div className="flex-1">Test</div>
+      <div id="map" className="h-[600px] w-full" ref={mapElement} />
     </div>
   );
 };
