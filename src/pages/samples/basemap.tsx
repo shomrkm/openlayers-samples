8e0d5486871766439d9a092c4bffe70eefb15708
shomrkm <shochan1985@gmail.com>
2023-02-18 10:52:07 +0900

    feat: Create a basemap page.

--- src/pages/samples/basemap.tsx
+++ src/pages/samples/basemap.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import 'ol/ol.css';
 import TileLayer from 'ol/layer/Tile';
@@ -8,21 +8,23 @@ import View from 'ol/View';
 
 const BaseMap: React.FC = () => {
   const mapElement = useRef<HTMLDivElement>(null);
-  const mapRef = useRef<OLMap>();
+  const [map, setMap] = useState<OLMap | undefined>();
 
   useEffect(() => {
-    if (!mapElement.current || mapRef.current) return;
-    mapRef.current = new OLMap({
+    if (!mapElement.current) return;
+    const initialMap = new OLMap({
       target: mapElement.current,
       view: new View({ projection: 'CRS:84', zoom: 16, center: [141.1378, 39.6987] }),
       layers: [new TileLayer({ source: new OSM() })],
     });
+    setMap(initialMap);
+    return () => initialMap.setTarget(undefined);
   }, []);
 
   return (
-    <div className="flex-col">
-      <div className="flex-1">Test</div>
-      <div id="map" className="h-[600px] w-full" ref={mapElement} />
+    <div className="m-4 flex-col">
+      <div className="text-4xl">Display Basemap</div>
+      <div className="mt-4 h-[600px] w-full" ref={mapElement} />
     </div>
   );
 };
