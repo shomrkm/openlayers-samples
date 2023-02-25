36e881c54f9f46ec0e4bc33636f3af45666bbaf9
shomrkm <shochan1985@gmail.com>
2023-02-25 01:46:42 +0000

    chore: Apply prettier formatter.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # openlayers-samples
+
 This is the project that demonstrate sample codes for openlayers(ol).
--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // swcMinify: false, 
+  // swcMinify: false,
   experimental: {
     appDir: true,
-  }
-}
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
