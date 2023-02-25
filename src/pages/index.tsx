e123569c96d1ab721e53db3578d5afb6ec2b9883
shomrkm <shochan1985@gmail.com>
2023-02-25 01:45:56 +0000

    feat: Create a main page layout.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,18 @@
 import Head from 'next/head';
+import { Card, CardProps } from '@/components/Card';
+
+const pages: CardProps[] = [
+  {
+    title: 'Show a Basemap',
+    description: 'Show a background map view like Open Street View.',
+    link: '/samples/Basemap',
+  },
+  {
+    title: 'Draw Shape',
+    description: 'Drawing a shape on the basemap.',
+    link: '/samples/DrawShape',
+  },
+];
 
 export default function Home() {
   return (
@@ -7,9 +21,19 @@ export default function Home() {
         <title>Openlayers Samples</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <div className="rounded-sm bg-red-100 p-4 text-3xl font-bold">Openlayers Samples</div>
-      </main>
+      <div className="m-2 h-screen w-screen flex-col bg-gray-100">
+        <div className="p-4 text-3xl font-bold text-gray-600">Openlayers Samples</div>
+        <div className="m-2 flex flex-wrap justify-start gap-2">
+          {pages.map((page) => (
+            <Card
+              key={page.title}
+              title={page.title}
+              description={page.description}
+              link={page.link}
+            />
+          ))}
+        </div>
+      </div>
     </>
   );
 }
